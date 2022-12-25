6feaf639ef1b20dd7eb4b785c084718c1ddb883a
Darya A <dashulyaalex@mail.ru>
2022-12-25 17:59:41 +0300

    4 step

---
+++ __tests__/index.test.js
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
+
+test('genDiff', () => {
+  expect(genDiff(file1, file2)).toEqual(expected);
+});
