82bbb27501f8a4ec9631281a35151934e1f87742
Darya A <dashulyaalex@mail.ru>
2023-01-05 19:29:30 +0300

    add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,10 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1>, <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   });
   
 
