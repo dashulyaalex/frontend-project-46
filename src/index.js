9335e2c39113a4446f2200db76058fb6c4acead7
Darya A <dashulyaalex@mail.ru>
2023-01-06 16:19:18 +0300

    step 7

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import parseFile from './parsers.js';
 import getDifference from './getDifference.js';
 import formatter from './formatters/index.js';
 
-const genDiff = (filePath1, filePath2, format = 'stylish') => {
+const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const obj1 = parseFile(filePath1);
   const obj2 = parseFile(filePath2);
   const data = getDifference(obj1, obj2);
-  return formatter(data, format);
+  return formatter(data, formatName);
 };
 
 export default genDiff;
