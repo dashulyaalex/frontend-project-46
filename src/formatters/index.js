e8dd3cc43c4142a9753aed1136dbe8ce1ecf0a55
Darya A <dashulyaalex@mail.ru>
2023-01-09 14:20:33 +0300

    changed formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,7 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const formatter = (data, formatName = 'stylish') => {
+const formatter = (data, formatName) => {
   switch (formatName) {
     case 'stylish':
       return stylish(data);
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,19 +8,16 @@ const stringify = (data) => {
 };
 
 const plain = (data, depth = 0) => {
-  // console.log(data);
   const lines = data.flatMap((currentLine) => {
     const nomination = depth ? `${depth}.${currentLine.key}` : `${currentLine.key}`;
     switch (currentLine.kind) {
       case 'added':
-        // console.log(currentLine.key);
         return `Property '${nomination}' was added with value: ${stringify(currentLine.value)}`;
       case 'deleted':
         return `Property '${nomination}' was removed`;
       case 'changed':
         return `Property '${nomination}' was updated. From ${stringify(currentLine.value1)} to ${stringify(currentLine.value2)}`;
       case 'nested':
-        // console.log(`!!!${nomination}`);
         return `${plain(currentLine.children, nomination)}`;
       case 'unchanged':
         return [];
